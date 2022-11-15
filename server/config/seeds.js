4739cd19518005bb4b759702837440f7d08b655b
Matthew Bascom <matt.bijan@gmail.com>
2022-11-15 14:31:30 -0600

    removed deletemany

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -2,7 +2,7 @@ const db = require('./connection');
 const { User, Product, Category } = require('../models');
 
 db.once('open', async () => {
-  await Category.deleteMany();
+  // await Category.deleteMany();
 
   const categories = await Category.insertMany([
     { name: 'Adventure' },
